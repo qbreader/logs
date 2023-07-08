5eca6db26630e7627e1a024d519f475c359a8880
Geoffrey Wu <geoffreywu1000@gmail.com>
2023-07-07 21:07:25 -0400

    apply formatter

--- singleplayer-packet/summarize.py
+++ singleplayer-packet/summarize.py
@@ -2,9 +2,9 @@ from collections import Counter
 from urllib.parse import unquote
 import regex
 
-f = open('raw.log', 'r', encoding='utf-8-sig')
-g = open('summary-packet.txt', 'w')
-h = open('summary-ip.txt', 'w')
+f = open("raw.log", "r", encoding="utf-8-sig")
+g = open("summary-packet.txt", "w")
+h = open("summary-ip.txt", "w")
 
 set_names = []
 ips = []
@@ -12,14 +12,14 @@ ips = []
 for line in f:
     line = line.strip()
 
-    if line in ['"message"', '']:
+    if line in ['"message"', ""]:
         continue
 
     try:
-        if 'status=404' in line:
+        if "status=404" in line:
             continue
 
-        set_name = regex.findall(r'(?<=setName=).*?(?=&)', line)[0]
+        set_name = regex.findall(r"(?<=setName=).*?(?=&)", line)[0]
         set_name = unquote(set_name)
 
         if len(set_name) == 0:
@@ -31,15 +31,15 @@ for line in f:
         print(line)
 
 counter = Counter(set_names)
-g.write('Set Name: # of packets requested\n')
-g.write(f'# of unique sets: {len(counter)} (total {len(set_names)} packets)\n')
-g.write('-----\n')
+g.write("Set Name: # of packets requested\n")
+g.write(f"# of unique sets: {len(counter)} (total {len(set_names)} packets)\n")
+g.write("-----\n")
 for key, value in counter.most_common():
-    g.write(f'{key:<50} {value}\n')
+    g.write(f"{key:<50} {value}\n")
 
 counter = Counter(ips)
-h.write('IP address: # of packets requested\n')
-h.write(f'# of unique IPs: {len(counter)}\n')
-h.write('-----\n')
+h.write("IP address: # of packets requested\n")
+h.write(f"# of unique IPs: {len(counter)}\n")
+h.write("-----\n")
 for key, value in counter.most_common():
-    h.write(f'{key:<20} {value}\n')
+    h.write(f"{key:<20} {value}\n")
